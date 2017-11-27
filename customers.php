dac81699cf50fb5e728c8b8ef580b142f1356728
David J. Thomas <dave.a.base@gmail.com>
2017-11-27 12:34:50 -0500

    bootstrapped

--- customers.php
+++ customers.php
@@ -22,43 +22,55 @@ $sql = "SELECT * FROM customers";
 // Send query to server and store returned data in $result
 $result = $conn->query($sql);
 
+echo $result;
 ?>
 
-<html>
+<html lang="en">
     <head>
         <title>Classic Models MySQL-PHP</title>
+        <!-- include Bootstrap CSS -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
     </head>
 
     <body>
-        <h1>Classic Models Customers</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>#</th><th>Name</th><th>Contact</th><th>Phone</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Country</th><th>Credit Limit</th>
-                </tr>
-            </thead>
-            <tbody>
-                    <?php
-                    // loop through each record, $row
-                    while($row = $result->fetch_assoc()) {
-                        // start a new table row for each record
-                        echo "<tr>";
-                        // write out each field as a cell
-                        echo "<td>" . $row["customerNumber"] . "</td>";
-                        echo "<td>" . $row["customerName"] . "</td>";
-                        echo "<td>" . $row["contactFirstName"] . " " . $row["contactLastName"] . "</td>";
-                        echo "<td>" . $row["phone"] . "</td>";
-                        echo "<td>" . $row["addressLine1"] . " " . $row["addressLine2"] . "</td>";
-                        echo "<td>" . $row["city"] . "</td>";
-                        echo "<td>" . $row["state"] . "</td>";
-                        echo "<td>" . $row["postalCode"] . "</td>";
-                        echo "<td>" . $row["country"] . "</td>";
-                        echo "<td>" . $row["creditLimie"] . "</td>";
-                        echo "</tr>";
-                    }
-                    ?>
-            </tbody>
-        </table>
+        <div class="jumbotron">Classic Models Customers</div>
+        <div class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th><th>Name</th><th>Contact</th><th>Phone</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Country</th><th>Credit Limit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <?php
+                        // loop through each record, $row
+                        while($row = $result->fetch_assoc()) {
+                            // start a new table row for each record
+                            echo "<tr>";
+                            // write out each field as a cell
+                            echo "<td>" . $row["customerNumber"] . "</td>";
+                            echo "<td>" . $row["customerName"] . "</td>";
+                            echo "<td>" . $row["contactFirstName"] . " " . $row["contactLastName"] . "</td>";
+                            echo "<td>" . $row["phone"] . "</td>";
+                            echo "<td>" . $row["addressLine1"] . " " . $row["addressLine2"] . "</td>";
+                            echo "<td>" . $row["city"] . "</td>";
+                            echo "<td>" . $row["state"] . "</td>";
+                            echo "<td>" . $row["postalCode"] . "</td>";
+                            echo "<td>" . $row["country"] . "</td>";
+                            echo "<td>" . $row["creditLimie"] . "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                </tbody>
+            </table>
+        </div>
+        <!-- include bootstrap javascript -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
 </html>
 
--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
-<html>
+<!-- You can use HTML just like normal on a .php document -->
+<html lang="en">
     <head>
         <title>Classic Models MySQL-PHP</title>
+        <!-- include Bootstrap CSS -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
     </head>
 
     <body>
-        <h1>Classic Models Database - PHP Example Pages</h1>
-        <ul>
-            <li><a href="customers.php">customers.php - Lists all customers</a></li>
-        </ul>
+        <!-- You can insert a PHP snippet anywhere, which can dynamically
+        generate html. The snipped below just prints the headline with 'echo'-->
+        <?php
+            # echo is even more powerful when used with variables and loops
+            echo "<div class=\"jumbotron\">Classic Models Database - PHP Example Pages</div>"
+        ?>
+        <div class="container">
+            <ul>
+                <li><a href="customers.php">customers.php - Lists all customers</a></li>
+            </ul>
+        </div>
+        <!-- include bootstrap javascript -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
 </html>
